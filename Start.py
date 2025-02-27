00626b1170b987e473fc5b7c00be6eb88f278e33
SavDor <savadoronin2013@gmail.com>
2025-02-27 22:47:52 +0700

    Add files via upload

---
+++ Start.py
@@ -0,0 +1,14 @@
+from colorama import init, Fore
+from colorama import Back
+from colorama import Style
+
+init(autoreset=True)
+
+print(Fore.CYAN +'███████████████████████████████████████')
+print(Fore.CYAN +'████████ ██████████  ██████████████████')
+print(Fore.CYAN +'███████████████████ █ █████████████████')
+print(Fore.CYAN +'████████ ██████████ █ █████████████████')
+print(Fore.CYAN +'████████ ██████████  ██████████████████')
+print(Fore.CYAN +'████████ ██████████ ███████████████████')
+print(Fore.CYAN +'████████ ██████████ ███████████████████')
+print(Fore.CYAN +'███████████████████████████████████████')
