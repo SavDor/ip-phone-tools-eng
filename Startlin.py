44be6b85bdd754f41c6f72831614f6c89a9d5ae7
SavDor <savadoronin2013@gmail.com>
2025-03-03 15:45:50 +0700

    Add files via upload

---
+++ Startlin.py
@@ -0,0 +1,44 @@
+from colorama import init, Fore
+from colorama import Back
+from colorama import Style
+import requests
+
+init(autoreset=True)
+
+import os
+clear = lambda: os.system('clear')
+clear()
+
+def Ip_For_Pc():
+    public_ip = requests.get('https://api.ipify.org').text
+    print(f"Public IP Address: {public_ip}")
+
+
+def Help():
+    print(Fore.CYAN +'███████████████████████████████████████')
+    print(Fore.CYAN +'████████ ██████████  ██████████████████')
+    print(Fore.CYAN +'███████████████████ █ █████████████████')
+    print(Fore.CYAN +'████████ ██████████ █ █████████████████')
+    print(Fore.CYAN +'████████ ██████████  ██████████████████')
+    print(Fore.CYAN +'████████ ██████████ ███████████████████')
+    print(Fore.CYAN +'████████ ██████████ ███████████████████')
+    print(Fore.CYAN +'███████████████████████████████████████')
+    print("")
+    print(Fore.CYAN +"1) Your Ip Address")
+    print(Fore.CYAN +"2) Help")
+    print(Fore.CYAN +"3) Exit")
+Help()
+
+while(True):
+    num1 = input(">> ")
+    if num1 == "3":
+        clear()
+        print(Fore.GREEN +"Goodbye")
+        quit()
+    elif num1 == "2":
+        clear()
+        Help()
+    elif num1 == "1":
+        Ip_For_Pc()
+    else:
+        print(Fore.RED +"No Command")
