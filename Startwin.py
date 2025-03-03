aa295ec98f941a1ccf8a1b2484536d1c965330d4
SavDor <savadoronin2013@gmail.com>
2025-03-03 17:28:07 +0700

    Add files via upload

---
+++ Startwin.py
@@ -0,0 +1,78 @@
+from colorama import init, Fore
+from colorama import Back
+from colorama import Style
+import os
+import requests
+import json
+from urllib.request import urlopen
+
+init(autoreset=True)
+
+clear = lambda: os.system('cls')
+
+clear()
+
+def Ip_For_Pc():
+    clear()
+    public_ip = requests.get('https://api.ipify.org').text
+    print(f"Public IP Address: {public_ip}")
+    print(Fore.CYAN + "y) More info")
+    print(Fore.CYAN + "else) Back")
+    num2 = input(Fore.MAGENTA +">> ")
+    if num2 == "y":
+        Ip_more_info()
+    else:
+        clear()
+        Help()
+
+
+def Ip_more_info():
+    url = 'http://ipinfo.io/json'
+    response = urlopen(url)
+    data = json.load(response)
+
+    IP=data['ip']
+    org=data['org']
+    city = data['city']
+    country=data['country']
+    region=data['region']
+    print(Fore.CYAN + "Ip - " + IP)
+    print(Fore.CYAN + "Org - " + org)
+    print(Fore.CYAN + "City - " + city)
+    print(Fore.CYAN + "Country - " + country)
+    print(Fore.CYAN + "Region - " + region)
+    print(Fore.CYAN + "1) Back") 
+    num3 = input(Fore.MAGENTA +">> ")
+    if num3 == "1":
+        clear()
+        Help()
+
+
+def Help():
+    print(Fore.CYAN +'███████████████████████████████████████')
+    print(Fore.CYAN +'████████ ██████████  ██████████████████')
+    print(Fore.CYAN +'███████████████████ █ █████████████████')
+    print(Fore.CYAN +'████████ ██████████ █ █████████████████')
+    print(Fore.CYAN +'████████ ██████████  ██████████████████')
+    print(Fore.CYAN +'████████ ██████████ ███████████████████')
+    print(Fore.CYAN +'████████ ██████████ ███████████████████')
+    print(Fore.CYAN +'███████████████████████████████████████')
+    print("")
+    print(Fore.CYAN +"1) Your Ip Address")
+    print(Fore.CYAN +"2) Help")
+    print(Fore.CYAN +"3) Exit")
+Help()
+
+while(True):
+    num1 = input(Fore.MAGENTA +">> ")
+    if num1 == "3":
+        print(Fore.GREEN +"Goodbye")
+        clear()
+        quit()
+    elif num1 == "2":
+        clear()
+        Help()
+    elif num1 == "1":
+        Ip_For_Pc()
+    else:
+        print(Fore.RED +"No Command")
